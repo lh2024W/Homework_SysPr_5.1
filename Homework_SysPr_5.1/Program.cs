f4770fa9bf09d887e8ab65dbbcf30592c037b5cf
Hacha_tq55@student.itstep.org <Hacha_tq55@student.itstep.org>
2024-10-03 20:56:56 +0300

    Добавьте файлы проекта.

---
+++ Homework_SysPr_5.1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Homework_SysPr_5._1
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //1. У вас есть массив целых чисел, и вам необходимо вычислить сумму всех элементов массива.
+            //Напишите параллельную программу для вычисления суммы с использованием нескольких объектов класса Task.
+            
+            int[] array = { 1, 2, 3, 4, 5 };
+
+            Parallel.ForEach(array, i =>
+            {
+                int res = SumNumbers(array);
+                Console.WriteLine("Сумма элементов массива: {0}", res);
+            });
+
+            //Parallel.For(0, array.Length, (i) =>
+            //{
+            //    int res = SumNumbers(array);
+            //    Console.WriteLine("Сумма элементов массива: {0}", res);
+            //});
+
+               
+        }
+
+        static int SumNumbers(int[] array )
+        {
+           int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            return sum;
+        }
+    }
+}
